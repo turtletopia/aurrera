6bbb6335dd79334146dcf7985de78febc32573ec
Laura <erdaradun.gaztea+github@gmail.com>
2023-02-05 12:47:50 +0100

    clean code a bit

--- R/progress_handler.R
+++ R/progress_handler.R
@@ -10,8 +10,9 @@ initialize_bar <- function(object, ...) {
 
 #' @importFrom utils getTxtProgressBar setTxtProgressBar
 advance_bar <- function(object, i) {
-  setTxtProgressBar(.PROGRESS_BAR, getTxtProgressBar(.PROGRESS_BAR) + 1)
-  if (getTxtProgressBar(.PROGRESS_BAR) == attr(.PROGRESS_BAR, "pb_length", exact = TRUE)) {
+  progress <- getTxtProgressBar(.PROGRESS_BAR)
+  setTxtProgressBar(.PROGRESS_BAR, progress + 1)
+  if (progress + 1 == attr(.PROGRESS_BAR, "pb_length", exact = TRUE)) {
     close(.PROGRESS_BAR)
     assignInMyNamespace(".PROGRESS_BAR", NULL)
   }
