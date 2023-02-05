b2fa11671cda5c91c8389e28a6a97c91bd588126
Laura <erdaradun.gaztea+github@gmail.com>
2023-02-05 13:33:30 +0100

    rename class to `aurrera_iter`

--- R/pb-methods.R
+++ R/pb-methods.R
@@ -1,17 +1,17 @@
 #' @export
-`[[.sp_iterable` <- function(x, i) {
+`[[.aurrera_iter` <- function(x, i) {
   advance_bar(x, i)
   NextMethod()
 }
 
 #' @export
-as.list.sp_iterable <- function(x, ...) {
+as.list.aurrera_iter <- function(x, ...) {
   pb(NextMethod())
 }
 
 #' @export
-str.sp_iterable <- function(object, ...) {
+str.aurrera_iter <- function(object, ...) {
   cat(sprintf("<aurrera iterable [%i]>\n", length(object)))
-  class(object) <- setdiff(class(object), "sp_iterable")
+  class(object) <- setdiff(class(object), "aurrera_iter")
   NextMethod()
 }
