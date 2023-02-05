1e3c7857c8cd9d9af11c9db7da7b8e392dc07081
Laura <erdaradun.gaztea+github@gmail.com>
2023-02-05 12:39:03 +0100

    implement custom str() method

--- R/pb-methods.R
+++ R/pb-methods.R
@@ -8,3 +8,10 @@
 as.list.sp_iterable <- function(x, ...) {
   pb(NextMethod())
 }
+
+#' @export
+str.sp_iterable <- function(object, ...) {
+  cat(sprintf("<aurrera iterable [%i]>\n", length(object)))
+  class(object) <- setdiff(class(object), "sp_iterable")
+  NextMethod()
+}
